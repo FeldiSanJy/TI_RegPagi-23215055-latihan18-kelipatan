f603fa6ecab0cd132e680f2bf080534e3cd20d34
FeldiSanJy <irenggeteng049@gmail.com>
2024-11-03 14:37:10 +0700

    Add files via upload

---
+++ kelipatan.java
@@ -0,0 +1,8 @@
+public class kelipatan {
+    public static void main(String[] args) {
+        // Loop dari 3.5 hingga 35 dengan kelipatan 3.5
+        for (double i = 3.5; i <= 35; i += 3.5) {
+            System.out.println(i);
+        }
+    }
+}
